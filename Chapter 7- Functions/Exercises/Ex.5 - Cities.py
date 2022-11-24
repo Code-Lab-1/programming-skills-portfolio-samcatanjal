ebb4300a977714654a6089e3f47af933b466ee2a
Samantha Catanjal <scatanjal@gmail.com>
2022-11-24 12:14:04 +0400

    Update

---
+++ Chapter 7- Functions/Exercises/Ex.5 - Cities.py
@@ -0,0 +1,9 @@
+from turtle import title
+
+
+def describe_city(city, country = 'Philippines'):
+    print(city + ' is in ' + country + '.')
+
+describe_city('Manila')
+describe_city('Quezon City')
+describe_city('Tokyo', 'Japan')
