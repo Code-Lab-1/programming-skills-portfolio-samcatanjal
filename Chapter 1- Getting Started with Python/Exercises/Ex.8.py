b40fb2ba7bc12b91c9217fa51675eeed69b8af07
Samantha Catanjal <scatanjal@gmail.com>
2022-11-25 14:51:46 +0400

    update

--- Chapter 1- Getting Started with Python/Exercises/Ex.8.py
+++ Chapter 1- Getting Started with Python/Exercises/Ex.8.py
@@ -1,5 +1,6 @@
-x = int(input())
-y = int(input())
-z = int(input())
+x = int(input("Input value for x: "))
+y = int(input("Input value for y: "))
+z = int(input("Input value for z: "))
 
+print('\nThe sum of the three numbers is:')
 print(x + y + z)
