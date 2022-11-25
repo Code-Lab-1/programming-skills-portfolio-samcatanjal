47bc9f33d854c92edffcd3badc35b0e2af352912
Samantha Catanjal <scatanjal@gmail.com>
2022-11-25 15:36:04 +0400

    update

--- Chapter 5- Dictionaries/Exercises/Ex.2 - Glossary.py
+++ Chapter 5- Dictionaries/Exercises/Ex.2 - Glossary.py
@@ -1,4 +1,10 @@
-glossary = {'Variable' : 'a reserved memory location to store values.', 'List': 'are used to store multiple items in a single variable', 'String': 'a series of characters.', 'Dictionary' : 'are used to store data values in key:value pairs.', 'Function' : 'a group of related statements that performs a specific task.' }
+glossary = {
+    'Variable' : 'a reserved memory location to store values.',
+    'List': 'are used to store multiple items in a single variable',
+    'String': 'a series of characters.',
+    'Dictionary' : 'are used to store data values in key:value pairs.',
+    'Function' : 'a group of related statements that performs a specific task.'
+}
 
 word = 'Variable'
 print(word + ": " + glossary[word])
--- Chapter 5- Dictionaries/Exercises/Ex.3 - Glossary 2.py
+++ Chapter 5- Dictionaries/Exercises/Ex.3 - Glossary 2.py
@@ -1,4 +1,11 @@
-glossary = {'Variable' : 'a reserved memory location to store values.', 'List': 'are used to store multiple items in a single variable', 'String': 'a series of characters.', 'Dictionary' : 'are used to store data values in key:value pairs.', 'Function' : 'a group of related statements that performs a specific task.' }
+glossary = {
+    'Variable' : 'a reserved memory location to store values.',
+    'List': 'are used to store multiple items in a single variable',
+    'String': 'a series of characters.',
+    'Dictionary' : 'are used to store data values in key:value pairs.',
+    'Function' : 'a group of related statements that performs a specific task.'
+}
+
 
 for keys, values in glossary.items():
     print('\n' + keys + ': ' + values)
