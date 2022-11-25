84ca6ae092c2601689ebbd021b08d61a284becff
Samantha Catanjal <scatanjal@gmail.com>
2022-11-25 15:29:43 +0400

    Update Ex.7.py

--- Chapter 4- Control Flow/Exercises/Ex.7.py
+++ Chapter 4- Control Flow/Exercises/Ex.7.py
@@ -1,6 +1,6 @@
 disney = input("What is your favorite disney movie? ")
 
-if disney == 'Peter Pan' or 'peter pan' or 'Peter pan' or 'Peterpan' or 'peterpan':
+if disney.title() == 'Peter Pan':
     print("Really? That's my favorite movie too!")
 else:
     print("Really? My favorite is Peter Pan.")
